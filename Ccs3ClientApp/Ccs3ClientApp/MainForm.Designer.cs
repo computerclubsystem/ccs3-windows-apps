baa802fa94783b1fc83da6c97c5191e30f24b459
computerclubsystem <email@email.email>
2025-01-18 22:56:47 +0200

    Try with another font to check strange letters on other computers

--- Ccs3ClientApp/Ccs3ClientApp/MainForm.Designer.cs
+++ Ccs3ClientApp/Ccs3ClientApp/MainForm.Designer.cs
@@ -47,80 +47,80 @@
             // lblRemainingTime
             // 
             lblRemainingTime.AutoSize = true;
-            lblRemainingTime.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblRemainingTime.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
             lblRemainingTime.Location = new Point(12, 9);
             lblRemainingTime.Name = "lblRemainingTime";
-            lblRemainingTime.Size = new Size(201, 32);
+            lblRemainingTime.Size = new Size(203, 27);
             lblRemainingTime.TabIndex = 0;
             lblRemainingTime.Text = "Оставащо време";
             // 
             // lblRemainingTimeValue
             // 
             lblRemainingTimeValue.AutoSize = true;
-            lblRemainingTimeValue.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblRemainingTimeValue.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
             lblRemainingTimeValue.Location = new Point(265, 9);
             lblRemainingTimeValue.Name = "lblRemainingTimeValue";
-            lblRemainingTimeValue.Size = new Size(27, 32);
+            lblRemainingTimeValue.Size = new Size(25, 27);
             lblRemainingTimeValue.TabIndex = 1;
             lblRemainingTimeValue.Text = "0";
             // 
             // lblStartedAt
             // 
             lblStartedAt.AutoSize = true;
-            lblStartedAt.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblStartedAt.Font = new Font("Arial", 18F);
             lblStartedAt.Location = new Point(12, 63);
             lblStartedAt.Name = "lblStartedAt";
-            lblStartedAt.Size = new Size(162, 32);
+            lblStartedAt.Size = new Size(163, 27);
             lblStartedAt.TabIndex = 2;
             lblStartedAt.Text = "Стартиран на";
             // 
             // lblStartedAtValue
             // 
             lblStartedAtValue.AutoSize = true;
-            lblStartedAtValue.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblStartedAtValue.Font = new Font("Arial", 18F);
             lblStartedAtValue.Location = new Point(268, 63);
             lblStartedAtValue.Name = "lblStartedAtValue";
-            lblStartedAtValue.Size = new Size(24, 32);
+            lblStartedAtValue.Size = new Size(20, 27);
             lblStartedAtValue.TabIndex = 3;
             lblStartedAtValue.Text = "-";
             // 
             // lblTotalTime
             // 
             lblTotalTime.AutoSize = true;
-            lblTotalTime.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblTotalTime.Font = new Font("Arial", 18F);
             lblTotalTime.Location = new Point(12, 124);
             lblTotalTime.Name = "lblTotalTime";
-            lblTotalTime.Size = new Size(225, 32);
+            lblTotalTime.Size = new Size(219, 27);
             lblTotalTime.TabIndex = 4;
             lblTotalTime.Text = "Използвано време";
             // 
             // lblTotalTimeValue
             // 
             lblTotalTimeValue.AutoSize = true;
-            lblTotalTimeValue.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblTotalTimeValue.Font = new Font("Arial", 18F);
             lblTotalTimeValue.Location = new Point(268, 124);
             lblTotalTimeValue.Name = "lblTotalTimeValue";
-            lblTotalTimeValue.Size = new Size(27, 32);
+            lblTotalTimeValue.Size = new Size(25, 27);
             lblTotalTimeValue.TabIndex = 5;
             lblTotalTimeValue.Text = "0";
             // 
             // lblTotalSum
             // 
             lblTotalSum.AutoSize = true;
-            lblTotalSum.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblTotalSum.Font = new Font("Arial", 18F);
             lblTotalSum.Location = new Point(12, 190);
             lblTotalSum.Name = "lblTotalSum";
-            lblTotalSum.Size = new Size(70, 32);
+            lblTotalSum.Size = new Size(71, 27);
             lblTotalSum.TabIndex = 6;
             lblTotalSum.Text = "Сума";
             // 
             // lblTotalSumValue
             // 
             lblTotalSumValue.AutoSize = true;
-            lblTotalSumValue.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblTotalSumValue.Font = new Font("Arial", 18F);
             lblTotalSumValue.Location = new Point(268, 190);
             lblTotalSumValue.Name = "lblTotalSumValue";
-            lblTotalSumValue.Size = new Size(58, 32);
+            lblTotalSumValue.Size = new Size(58, 27);
             lblTotalSumValue.TabIndex = 7;
             lblTotalSumValue.Text = "0.00";
             // 
@@ -128,7 +128,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(441, 259);
+            ClientSize = new Size(638, 259);
             Controls.Add(lblTotalSumValue);
             Controls.Add(lblTotalSum);
             Controls.Add(lblTotalTimeValue);
