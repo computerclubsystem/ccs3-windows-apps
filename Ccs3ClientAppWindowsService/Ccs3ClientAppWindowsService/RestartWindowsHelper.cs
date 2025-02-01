264eca687e9841972858c8e151482e8d981c5a6e
computerclubsystem <email@email.email>
2025-02-01 19:07:21 +0200

    Change restart method to use  InitiateSystemShutdownExA #8

--- Ccs3ClientAppWindowsService/Ccs3ClientAppWindowsService/RestartWindowsHelper.cs
+++ Ccs3ClientAppWindowsService/Ccs3ClientAppWindowsService/RestartWindowsHelper.cs
@@ -4,21 +4,24 @@ namespace Ccs3ClientAppWindowsService;
 
 public class RestartWindowsHelper {
     [DllImport("user32.dll", SetLastError = true)]
-    static extern int ExitWindowsEx(uint uFlags, uint dwReason);
+    static extern bool InitiateSystemShutdownExA(
+      string? lpMachineName,
+      string? lpMessage,
+      int dwTimeout,
+      bool bForceAppsClosed,
+      bool bRebootAfterShutdown,
+      uint dwReason
+    );
 
     public void Restart() {
 #if DEBUG
         return;
 #endif
-        const ushort EWX_REBOOT = 0x00000002;
-        const ushort EWX_FORCE = 0x00000004;
-        const uint exitFlags = EWX_REBOOT | EWX_FORCE;
-
         const uint SHTDN_REASON_MAJOR_APPLICATION = 0x00040000;
         const uint SHTDN_REASON_MINOR_MAINTENANCE = 0x00000001;
         const uint SHTDN_REASON_FLAG_PLANNED = 0x80000000;
         const uint reasonFlags = SHTDN_REASON_MAJOR_APPLICATION | SHTDN_REASON_MINOR_MAINTENANCE | SHTDN_REASON_FLAG_PLANNED;
-        
-        ExitWindowsEx(exitFlags, reasonFlags);
+
+        InitiateSystemShutdownExA(null, null, 0, true, true, reasonFlags);
     }
 }
